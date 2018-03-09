388f69f6d4eeea9e140435c31b6ddfcdee0655d7
milenacard <mile.card195@gmail.com>
2018-03-09 10:15:50 -0500

    Configuration route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react'
-import { Route } from 'react-router-dom'
-// install depedency react-router-dom
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Welcome from './components/Welcome.js'
 
 export default function App (props) {
   return (
-    <Fragment>
-        <Route exact path='/'>
-          render = {() => <Welcome name={props.data.name} /> }
-        </Route>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Route exact path='/'
+          render={() => <Welcome name={props.data.name} />} />
+      </Fragment>
+    </Router>
   )
 }
