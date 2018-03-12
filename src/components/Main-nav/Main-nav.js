3b158c7c948c3ff1968bf4a343a6e01238bb6cf8
milenacard <mile.card195@gmail.com>
2018-03-12 12:26:10 -0400

    DO: DropdownList and hamburger Icon

--- src/components/Main-nav/Main-nav.js
+++ src/components/Main-nav/Main-nav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import './Main-nav.scss'
 
 export default class MainNav extends Component {
@@ -10,7 +10,6 @@ export default class MainNav extends Component {
     }
 
     this.toggle = this.toggle.bind(this)
-    this.data = props.data
   }
 
   toggle () {
@@ -24,20 +23,56 @@ export default class MainNav extends Component {
       open: this.state.open ? 'nav-bar__hamburger--clicked' : ''
     }
 
+    const activeList = {
+      open: this.state.open ? 'nav-bar__list--activate' : ''
+    }
+
     return (
       <nav className='nav-bar'>
-        <MainNav className='nav-bar__container' >
+        <section className='nav-bar__container' >
           <button className={`nav-bar__hamburger ${activeHamburger.open}`} onClick={this.toggle}>
             <div className='nav-bar__hamburger-line nav-bar__hamburger-line-first' />
             <div className='nav-bar__hamburger-line nav-bar__hamburger-line-middle' />
             <div className='nav-bar__hamburger-line nav-bar__hamburger-line-last' />
           </button>
           <div className='nav-bar__logo-container'>
-            <a className='nav-bar__logo-link'>
-              <img className='nav-bar__logo-image' />
+            <a className='nav-bar__logo-link' href={this.props.data.logo.link}>
+              <img className='nav-bar__logo-image' src={this.props.data.logo.img} />
             </a>
-          </div>
-        </MainNav>
+         // </div>
+        </section>
+        <ul className={`nav-bar__list ${activeList.open}`}>
+          {
+            this.props.data.links.map(element => {
+              if (element.hasOwnProperty('href')) {
+                return (
+                  <li key={element.label + element.href} className='nav-bar__list-item'>
+                    <a className='nav-bar__list__link-item' />
+                  </li>
+                )
+              } else {
+                return (
+                  <Fragment>
+                    <li className='nav-bar__list-item'>
+                      <button className='nav-bar__list-item-button'>{element.label}</button>
+                      <ul className='nav-bar__list__link'>
+                        {
+                          element.links.map(subelement => {
+                            return (
+                              <li key={subelement.label + subelement.href} className='nav-bar__list__item-link'>
+                                <a className='nav-bar__list__subitem-link' href={subelement.href}>{element.label}</a>
+                              </li>
+                            )
+                          })
+                        }
+                      </ul>
+                    </li>
+                  </Fragment>
+                )
+              }
+            })
+          }
+        </ul>
       </nav>
     )
   }
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { render } from 'react-dom'
 
 import App from './App'
 import data from '../data/data.json'
+import '../node_modules/reset-css/_reset.scss'
 
 render(<App data={data} />, document.getElementById('app'))
