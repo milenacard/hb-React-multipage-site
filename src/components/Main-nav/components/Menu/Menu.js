c428806cb414763dbd67819e4e58c142f591fb4b
milenacard <mile.card195@gmail.com>
2018-03-12 14:14:03 -0400

    Add component List and Menu

--- src/components/Main-nav/components/Menu/Menu.js
+++ src/components/Main-nav/components/Menu/Menu.js
@@ -1,32 +1,23 @@
 import React from 'react'
 import './Menu.scss'
 
-export default function Menu ({data}) {
+import List from '../List/List'
+
+export default function Menu ({data, className}) {
   return (
-    <ul>
+    <ul className={className}>
       {
         data.links.map(element => {
           if (element.hasOwnProperty('href')) {
             return (
               <li key={element.label + element.href} className='nav-bar__list-item'>
-                <a className='nav-bar__list__link-item' />
+                <a className='nav-bar__list__link-item' href={element.href}>{element.label}</a>
               </li>
             )
           } else {
             return (
               <li key={element.label + element.href} className='nav-bar__list-item'>
-                <button className='nav-bar__list-item-button'>{element.label}</button>
-                <ul className='nav-bar__list__link'>
-                  {
-                    element.links.map(subelement => {
-                      return (
-                        <li key={subelement.label + subelement.href} className='nav-bar__list__item-link'>
-                          <a className='nav-bar__list__subitem-link' href={subelement.href}>{element.label}</a>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
+                <List data={element} />
               </li>
             )
           }
