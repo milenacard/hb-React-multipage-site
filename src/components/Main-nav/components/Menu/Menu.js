bf964f4799cefdfadb7730fe1ed652bcc8d166ae
milenacard <mile.card195@gmail.com>
2018-03-13 13:12:34 -0400

    DO: array for MenuItem TODO: collapse method, capture event onclik

--- src/components/Main-nav/components/Menu/Menu.js
+++ src/components/Main-nav/components/Menu/Menu.js
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Menu.scss'
 
 import List from '../List/List'
 
-export default function Menu ({data, className}) {
-  return (
-    <ul className={className}>
-      {
-        data.links.map(element => {
-          if (element.hasOwnProperty('href')) {
-            return (
-              <li key={element.label + element.href} className='nav-bar__list-item'>
-                <a className='nav-bar__list__link-item' href={element.href}>{element.label}</a>
-              </li>
-            )
-          } else {
-            return (
-              <li key={element.label + element.href} className='nav-bar__list-item'>
-                <List data={element} />
-              </li>
-            )
-          }
-        })
-      }
-    </ul>
-  )
+export default class Menu extends Component {
+  constructor (props) {
+    super(props)
+
+    this.arrayItem = []
+  }
+
+  render () {
+    return (
+      <ul className={this.props.className}>
+        {
+          this.props.data.links.map(element => {
+            if (element.hasOwnProperty('href')) {
+              return (
+                <li key={element.label + element.href} className='nav-bar__list-item'>
+                  <a className='nav-bar__list__link-item' href={element.href}>{element.label}</a>
+                </li>
+              )
+            } else {
+              return (
+                <li key={element.label + element.href} className='nav-bar__list-item'>
+                  <List data={element}
+                    ref={(list) => { this.arrayItem.push(list) }} />
+                </li>
+              )
+            }
+          })
+        }
+      </ul>
+    )
+  }
 }
